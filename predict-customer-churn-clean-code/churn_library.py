ab2ab4bce36839054fe926c6f40004546d822e1c
patrick brus <patrick.brus@infineon.com>
2024-02-12 17:17:51 +0100

    Feat: Continued coding for final project clean code

--- predict-customer-churn-clean-code/churn_library.py
+++ predict-customer-churn-clean-code/churn_library.py
@@ -1,5 +1,6 @@
 # library doc string
 
+from __future__ import annotations
 
 # import libraries
 import os
@@ -8,7 +9,16 @@ import pandas as pd
 import shutil
 import seaborn as sns
 import matplotlib.pyplot as plt
-os.environ['QT_QPA_PLATFORM']='offscreen'
+from sklearn.preprocessing import normalize
+from sklearn.model_selection import train_test_split
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.metrics import plot_roc_curve, classification_report
+
+os.environ["QT_QPA_PLATFORM"]="offscreen"
 
 
 
@@ -26,8 +36,21 @@ def import_data(pth: str) -> pd.core.frame.DataFrame :
     """
     if not os.path.exists(pth):
         raise FileNotFoundError(f"The file at path {pth} does not exist.")
-    return pd.read_csv(pth)
+    return add_target_col_to_df(pd.read_csv(pth))
+    
 
+def add_target_col_to_df(df: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
+    """Helper function to add the target column with name Churn which will be used for training later.
+
+    Args:
+        df (pd.core.frame.DataFrame): pandas dataframe
+
+    Returns:
+        pd.core.frame.DataFrame: pandas dataframe with added column Churn
+    """
+    df["Churn"] = df["Attrition_Flag"].apply(lambda val: 0 if val == "Existing Customer" else 1)
+    return df
+    
 
 def perform_eda(df: pd.core.frame.DataFrame, path_plots="images/eda"):
     """ Perform eda on df and save figures to images folder.
@@ -52,51 +75,80 @@ def perform_eda(df: pd.core.frame.DataFrame, path_plots="images/eda"):
             ax.hist(df[feature])
             filename = f"hist_{feature}.png"
         else:
-            df[feature].value_counts("normalize").plot(kind='bar', ax=ax)
+            df[feature].value_counts("normalize").plot(kind="bar", ax=ax)
             filename = f"bar_{feature}.png"
         
         ax.set_title(feature)
-        ax.set_xlabel('Value')  # add a label to the x axis
-        ax.set_ylabel('Frequency')  # add a label to the y axis
+        ax.set_xlabel("Value")  # add a label to the x axis
+        ax.set_ylabel("Frequency")  # add a label to the y axis
         plt.tight_layout()
         fig.savefig(os.path.join(path_plots, filename), dpi=300)
         plt.close(fig)  # close the figure to avoid memory leaks
     
     # create and plot heatmap
     fig = plt.figure(figsize=(20,10)) 
-    sns.heatmap(df.corr(), annot=False, cmap='Dark2_r', linewidths = 2)
+    sns.heatmap(df.corr(), annot=False, cmap="Dark2_r", linewidths = 2)
     fig.savefig(os.path.join(path_plots, "feature_heatmap.png"), dpi=300)
     
 
 
-def encoder_helper(df, category_lst, response):
-    '''
+def encoder_helper(df: pd.core.frame.DataFrame, category_lst: list[str], response: str) -> pd.core.frame.DataFrame:
+    """
     helper function to turn each categorical column into a new column with
     propotion of churn for each category - associated with cell 15 from the notebook
 
-    input:
-            df: pandas dataframe
-            category_lst: list of columns that contain categorical features
-            response: string of response name [optional argument that could be used for naming variables or index y column]
+    Args:
+        df (pd.core.frame.DataFrame): pandas dataframe
+        category_lst: list of columns that contain categorical features
+        response: string of response name [optional argument that could be used for naming variables or index y column]
 
-    output:
-            df: pandas dataframe with new columns for
-    '''
-    pass
+    Returns:
+        df (pd.core.frame.DataFrame): pandas dataframe with new columns for
+    """
+    for category in category_lst:
+        logging.info(f"Add proportion of churn for category {category}...")
+        category_mean = df.groupby(category)["Churn"].transform("mean")
+        df[category + "_Churn"] = category_mean
+    
+    return df.drop(category_lst, axis=1)
 
 
-def perform_feature_engineering(df, response):
-    '''
-    input:
-              df: pandas dataframe
-              response: string of response name [optional argument that could be used for naming variables or index y column]
+def perform_feature_engineering(df: pd.core.frame.DataFrame, response):
+    """
+    apply feature engineering on dataset before training in order to prepare training dataset
+    Args:
+        df (pd.core.frame.DataFrame): pandas dataframe
+        response: string of response name [optional argument that could be used for naming variables or index y column]
 
-    output:
-              X_train: X training data
-              X_test: X testing data
-              y_train: y training data
-              y_test: y testing data
-    '''
+    Returns:
+        X_train: X training data
+        X_test: X testing data
+        y_train: y training data
+        y_test: y testing data
+    """
+    cat_cols = get_categorical_cols(df)
+    df = encoder_helper(df, category_lst=cat_cols, response=response)
+    
+    y = df[response]
+    X = df.drop(response, axis=1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.3, random_state=42)
+    
+    return X_train, X_test, y_train, y_test
+
+def get_categorical_cols(df):
+    """Get the list of categorical columns in the dataframe.
+    
+    Args:
+    df (pandas DataFrame): Input dataframe.
+    
+    Returns:
+    list: List of categorical columns.
+    """
+    cat_cols = []
+    for col in df.columns:
+        if df[col].dtype == 'object' or pd.api.types.is_categorical_dtype(df[col]):
+            cat_cols.append(col)
+    return cat_cols
 
 def classification_report_image(y_train,
                                 y_test,
@@ -104,49 +156,78 @@ def classification_report_image(y_train,
                                 y_train_preds_rf,
                                 y_test_preds_lr,
                                 y_test_preds_rf):
-    '''
+    """
     produces classification report for training and testing results and stores report as image
     in images folder
-    input:
-            y_train: training response values
-            y_test:  test response values
-            y_train_preds_lr: training predictions from logistic regression
-            y_train_preds_rf: training predictions from random forest
-            y_test_preds_lr: test predictions from logistic regression
-            y_test_preds_rf: test predictions from random forest
-
-    output:
-             None
-    '''
+    Args:
+        y_train: training response values
+        y_test:  test response values
+        y_train_preds_lr: training predictions from logistic regression
+        y_train_preds_rf: training predictions from random forest
+        y_test_preds_lr: test predictions from logistic regression
+        y_test_preds_rf: test predictions from random forest
+    """
     pass
 
 
 def feature_importance_plot(model, X_data, output_pth):
-    '''
+    """
     creates and stores the feature importances in pth
-    input:
-            model: model object containing feature_importances_
-            X_data: pandas dataframe of X values
-            output_pth: path to store the figure
-
-    output:
-             None
-    '''
+    Args:
+        model: model object containing feature_importances_
+        X_data: pandas dataframe of X values
+        output_pth: path to store the figure
+    """
     pass
 
 def train_models(X_train, X_test, y_train, y_test):
-    '''
+    """
     train, store model results: images + scores, and store models
-    input:
-              X_train: X training data
-              X_test: X testing data
-              y_train: y training data
-              y_test: y testing data
-    output:
-              None
-    '''
-    pass
+    Args:
+        X_train: X training data
+        X_test: X testing data
+        y_train: y training data
+        y_test: y testing data
+    """
+    rfc = RandomForestClassifier(random_state=42)
+    lrc = LogisticRegression(solver="lbfgs", max_iter=3000)
+
+    # dict for grid search parameters
+    param_grid = { 
+        "n_estimators": [200, 500],
+        "max_features": ["auto", "sqrt"],
+        "max_depth" : [4,5,100],
+        "criterion" :["gini", "entropy"]
+    }
+
+    # Train random forest model using grid seach
+    cv_rfc = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=5)
+    cv_rfc.fit(X_train, y_train)
+    
+    # Train logistic regression model
+    lrc.fit(X_train, y_train)
+    
+    # get predictions 
+    y_train_preds_rf = cv_rfc.best_estimator_.predict(X_train)
+    y_test_preds_rf = cv_rfc.best_estimator_.predict(X_test)
+    y_train_preds_lr = lrc.predict(X_train)
+    y_test_preds_lr = lrc.predict(X_test)
+    
+    classification_report_image(y_train,
+                                y_test,
+                                y_train_preds_lr,
+                                y_train_preds_rf,
+                                y_test_preds_lr,
+                                y_test_preds_rf)
+    
+    
 
 if __name__ == "__main__":
     data = import_data(r"./data/bank_data.csv")
     perform_eda(data)
+    X_train, X_test, y_train, y_test = perform_feature_engineering(data, response="Churn")
+    
+    print(X_train.shape)
+    print(X_test.shape)
+    print(y_train.shape)
+    print(y_test.shape)
--- predict-customer-churn-clean-code/test_churn_script_logging_and_tests.py
+++ predict-customer-churn-clean-code/test_churn_script_logging_and_tests.py
@@ -1,14 +1,15 @@
 import logging
 import pytest
+import numpy as np
 import pandas as pd
 from unittest.mock import patch
 from churn_library import *
 
 logging.basicConfig(
-    filename='./logs/churn_library.log',
+    filename="./logs/churn_library.log",
     level = logging.INFO,
-    filemode='w',
-    format='%(name)s - %(levelname)s - %(message)s')
+    filemode="w",
+    format="%(name)s - %(levelname)s - %(message)s")
 
 @pytest.fixture
 def path():
@@ -18,10 +19,14 @@ def path():
     return r"./data/bank_data.csv"
 
 @pytest.fixture
+def df_test_churn():
+    return pd.DataFrame({"Attrition_Flag": ["Existing Customer", "Existing Customer", "Attrited Customer"]})
+
+@pytest.fixture
 def sample_df():
-    return pd.DataFrame({'col1': [1, 2, 3, 4, 5], 
-                         'col2': [2, 4, 6, 8, 10],
-                         'col3': ['A', 'B', 'C', 'D', 'E']})
+    return pd.DataFrame({"col1": [1, 2, 3, 4, 5], 
+                         "col2": [2, 4, 6, 8, 10],
+                         "col3": ["A", "B", "C", "D", "E"]})
 
 def test_import_data_returns_dataframe(path):
     data = import_data(path)
@@ -29,25 +34,62 @@ def test_import_data_returns_dataframe(path):
     
 def test_import_data_raises_exception():
     with pytest.raises(FileNotFoundError):
-        import_data('non_existent_file.csv')
+        import_data("non_existent_file.csv")
         
 def test_import_data_reads_csv(path):
     with patch("pandas.read_csv") as mock_csv:
         import_data(path)
         mock_csv.assert_called_once_with(path)
+
+def test_add_churn_col():
+    df_test_churn = pd.DataFrame({"Attrition_Flag": ["Existing Customer", "Existing Customer", "Attrited Customer"]})
+    extended_df = add_target_col_to_df(df_test_churn)
+    
+    assert extended_df["Churn"].tolist() == [0, 0, 1]
         
 def test_perform_eda(sample_df):
-    # create temporary directory for test images
-    test_path_plots = 'test_images/eda'
-
-    # run the function
+    test_path_plots = "test_images/eda"
     perform_eda(sample_df, test_path_plots)
 
-    # check that the expected files have been created
     assert os.path.exists(os.path.join(test_path_plots, "hist_col1.png"))
     assert os.path.exists(os.path.join(test_path_plots, "hist_col2.png"))
     assert os.path.exists(os.path.join(test_path_plots, "bar_col3.png"))
     assert os.path.exists(os.path.join(test_path_plots, "feature_heatmap.png"))
 
-    # cleanup the directory after the test
-    shutil.rmtree(test_path_plots)        
+    shutil.rmtree(test_path_plots)
+    
+def test_get_categorical_cols():
+    df = pd.DataFrame({"col1": ["A", "B", "C"], 
+                       "col2": [1, 2, 3], 
+                       "col3": pd.Categorical(["X", "Y", "X"])})
+    
+    assert get_categorical_cols(df) == ["col1", "col3"]
+
+def test_encoder_helper():
+    df = pd.DataFrame({"Gender": ["M", "F", "M", "M", "F"], 
+                       "Churn": [1, 0, 1, 1, 1],
+                       "num_col1": [1, 2, 3, 4, 5]})
+    
+    df_encoded = encoder_helper(df, ["Gender"], "Churn")
+    
+    assert sorted(df_encoded.columns) == ['Churn', 'Gender_Churn', 'num_col1']
+    assert (df_encoded["Gender_Churn"] == [1.0, 0.5, 1.0, 1.0, 0.5]).all()
+    
+def test_perform_feature_engineering():
+    # Test perform_feature_engineering function
+    # Create a sample dataframe
+    df = pd.DataFrame({'cat_col1': ['A', 'B', 'C'], 
+                       'cat_col2': ['X', 'Y', 'X'],
+                       'num_col1': [1, 2, 3],
+                       'Churn': [1, 0, 1]})
+    
+    # Test the function output
+    X_train, X_test, y_train, y_test = perform_feature_engineering(df, 'Churn')
+    assert isinstance(X_train, pd.DataFrame)
+    assert isinstance(X_test, pd.DataFrame)
+    assert isinstance(y_train, pd.Series)
+    assert isinstance(y_test, pd.Series)
+    assert len(X_train) == 2
+    assert len(X_test) == 1
+    assert len(y_train) == 2
+    assert len(y_test) == 1
